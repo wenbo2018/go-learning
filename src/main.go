f4d76a81b038ba17b774fc3b67ffe105b7418e14
shenwenbo <wenbo.shen@wenboshendeMacBook-Pro.local>
2018-03-29 19:50:59 +0800

    add server

--- src/main.go
+++ src/main.go
@@ -2,48 +2,23 @@ package main
 
 import (
 	"net/http"
-	"log"
 	"fmt"
-	"sync"
 )
 
-var mu sync.Mutex
-var count int
+type MyMux struct{}
 
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhelloName(w, r)
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello myroute!")
+}
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/head", handlerHttpHead)
-	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	mux := &MyMux{}
+	http.ListenAndServe(":9090", mux)
 }
-// handler echoes the Path component of the requested URL.
-func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
-// counter echoes the number of calls so far.
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
-}
-
-
-// handler echoes the HTTP request.
-func handlerHttpHead(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
-}
-
